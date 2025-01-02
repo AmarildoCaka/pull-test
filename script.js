6100a81496fc11fa5e06da70721fea3ae99e928a
AmarildoCaka <139240882+AmarildoCaka@users.noreply.github.com>
2025-01-02 19:28:59 +0100

    added

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ alertThingsFunct();
 const a = true;
 
 const b = false;
+
+const arrayMecha = () => {
+
+  const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  testArray.forEach((data) => {
+
+    console.log(testArray[data]);
+
+  });
+
+}
+
+arrayMecha();
